0a49f2dd947185edc7dd4057ed4d97080a5e111a
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-05-14 12:56:51 +0600

    navbar related work on going

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import Image from "next/image";
+
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className="flex justify-between bg-white p-5">
+            <ul className="flex gap-5">
+                <li><link  href={'/'} />Home</li>
+                <li><link  href={'/destinations'} />Destinations</li>
+                <li><link  href={'/my-bookings'} />My-bookings</li>
+                
+            </ul>
+
+            <div>
+                <Image src={'/assets/Wanderlast.png'}
+                height={150}
+                width={150}
+                alt="logo"
+                ></Image>
+            </div>
+
+            <ul className="flex gap-5 bg-white p-5">
+                <li><link  href={'/profile'} />Profile</li>
+                <li><link  href={'/login'} />Login</li>
+                <li><link  href={'/signup'} />Sign-Up</li>
+                
+            </ul>
+        </nav>
     );
 };
 
