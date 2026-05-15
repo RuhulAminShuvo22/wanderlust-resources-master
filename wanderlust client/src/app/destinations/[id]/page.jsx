4b657e1d872f18b988dc45075ce72123a5a8432a
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-05-15 16:30:21 +0600

    Destination Details Page more update

--- wanderlust client/src/app/destinations/[id]/page.jsx
+++ wanderlust client/src/app/destinations/[id]/page.jsx
@@ -1,4 +1,10 @@
 import Image from "next/image";
+import { FaExternalLinkAlt } from "react-icons/fa";
+
+
+import { RiMapPin2Fill } from "react-icons/ri";
+import { SlCalender } from "react-icons/sl";
+import Link from "next/link";
 
 
 const DestinationDetailsPage = async({params}) => {
@@ -9,7 +15,7 @@ const DestinationDetailsPage = async({params}) => {
     const destination = await res.json();
     //console.log(destination)
 
-    const {imageUrl, price, destinationName, duration, country } = destination;
+    const {imageUrl, price, destinationName, duration, country, description } = destination;
 
 
 
@@ -28,6 +34,43 @@ const DestinationDetailsPage = async({params}) => {
 
 
             </Image>
+
+            <div className="p-2">
+                            <div className="flex items-center gap-1">
+                                <RiMapPin2Fill />
+                                <span>{country}</span>
+                            </div>
+            
+                            <div className="flex justify-between items-center mt-2">
+                                <div>
+                                    <h2 className="text-xl font-bold">
+                                        {destinationName}
+                                    </h2>
+                                </div>
+            
+                                <div className="flex gap-2 items-center">
+                                    <SlCalender />
+                                    {duration}
+                                </div>
+            
+                                <div className="text-2xl font-bold">
+                                    <h3>${price}</h3>
+                                </div>
+    
+                            </div>
+
+                            <div>
+                                <h2 className="mt-10 text-2xl font-bold">OverView</h2>
+                                <p>{description}</p>
+
+                                <br />
+
+                                <h2 className="mt-10 text-2xl font-bold">Highlight</h2>
+                                
+                            </div>
+                            
+                            
+                        </div>
 
 
 
