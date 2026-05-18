6310bf52f436b20069c4eb6e9ea330ddba41d5cb
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-05-18 16:27:09 +0600

    my booking page work on going

--- wanderlust client/src/app/my-bookings/page.jsx
+++ wanderlust client/src/app/my-bookings/page.jsx
@@ -1,5 +1,6 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+import Image from "next/image";
 
 
 const MyBookingsPage = async () => {
@@ -14,13 +15,30 @@ const MyBookingsPage = async () => {
 
     const res = await fetch(`http://localhost:5000/booking/${user?.id}`);
 
-    const data = await res.json()
-    console.log(data)
+    const bookings = await res.json()
+    console.log(bookings)
 
 
     return (
         <div className='max-w-7xl mx-auto'>
             <h1 className='text-3xl font-bold'> My Bookings </h1>
+
+            <div>
+                {
+                    bookings.map(booking => 
+                        <div key={booking._id}>
+                            <Image 
+                                src={booking.imageUrl}
+                                alt={booking.destinationName}
+                                height={300}
+                                width={300}
+                                >
+
+                            </Image>
+                        </div>)
+                }
+            </div>
+
         </div>
     );
 };
