ccd6da0e490ec1b0e39cad3680b8f83072e472a1
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-05-19 01:55:35 +0600

    more updae on proxy section

--- wanderlust client/src/proxy.js
+++ wanderlust client/src/proxy.js
@@ -2,20 +2,41 @@ import { NextResponse } from "next/server";
 import { auth } from "./lib/auth";
 import { headers } from "next/headers";
 
-// This function can be marked `async` if using `await` inside
+// Next.js 16-এর নতুন নিয়ম অনুযায়ী ফাংশনের নাম অবশ্যই 'proxy' হতে হবে
 export async function proxy(request) {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
+  // ১. ইউজার লগইন না থাকলে তাকে সরাসরি '/login' পেজে পাঠিয়ে দিন
   if (!session) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  return NextResponse.redirect(new URL("/home", request.url));
+  // ২. ইউজার লগইন থাকলে তাকে তার কাঙ্ক্ষিত পেজেই যেতে দিন 
+  // জোর করে অন্য কোথাও রিডাইরেক্ট করার প্রয়োজন নেই, এতে ৪MD বা লুপ হবে না
+  return NextResponse.next();
 }
 
 export const config = {
-  //matcher: '/about/:path*',
-  matcher: ["/my-bookings", "/add-destination", "/profile"],
+  // যেসব পেজ আপনি লগইন ছাড়া লক রাখতে চান
+  matcher: [
+    "/my-bookings",
+    "/add-destination",
+    "/destinations/:path*", // :path* দিলে ডাইনামিক রাউট সাব-পেজসহ লক হবে
+    "/profile",
+  ],
 };
+
+
+
+// import { NextResponse } from 'next/server'
+ 
+// // This function can be marked `async` if using `await` inside
+// export function proxy(request) {
+//   return NextResponse.redirect(new URL('/home', request.url))
+// }
+ 
+// export const config = {
+//   matcher: '/about/:path*',
+// }
